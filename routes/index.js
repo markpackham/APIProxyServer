f01cee93d034363c9a06dbb21ffbd216d98a2b1d
mark <markpackham1@gmail.com>
2021-10-28 10:23:42 +0100

    add needle
    https://youtu.be/ZGymN8aFsv4?t=681

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+// needle would be doing a job simular to node fetch but you can use common JS syntax
+const needle = require("needle");
 
 router.get("/", (req, res) => {
-  res.json({ success: true });
+  //   res.json({ success: true });
 });
 
 module.exports = router;
