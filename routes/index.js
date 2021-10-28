6d3437b3eaf64ddbeffb31340abe99922c7f6b29
mark <markpackham1@gmail.com>
2021-10-28 11:42:36 +0100

    setup query for API
    https://youtu.be/ZGymN8aFsv4?t=1084

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const url = require("url");
 const express = require("express");
 const router = express.Router();
 // needle would be doing a job simular to node fetch but you can use common JS syntax
@@ -19,8 +20,10 @@ router.get("/", async (req, res) => {
   try {
     const params = new URLSearchParams({
       [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query,
     });
 
+    // URLSearchParams saves us from having to write a very ugly looking url
     const apiRes = await needle("get", `${API_BASE_URL}?${params}`);
     const data = apiRes.body;
     res.status(200).json(data);
