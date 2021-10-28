60ae05c64a595238afbb7395bc6f137b1f7f246e
mark <markpackham1@gmail.com>
2021-10-28 11:56:50 +0100

    set rate limit
    https://youtu.be/ZGymN8aFsv4?t=1365

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Rate limiting
+// When you go to dev tools in the Header section of your browser you can see
+// X-RateLimit-Limit and X-RateLimit-Remaining
+const limiter = rateLimit({
+  //Only 5 requests allowed in 10 minutes
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+});
+
+app.use(limiter);
+app.set("trust proxy", 1);
 
 // Routes
 // since we want ./routes/index we can leave out the "index" bit
