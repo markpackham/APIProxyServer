d8617b6c3e2e11d6bfe7b16d7eca9fb01a6c4b43
mark <markpackham1@gmail.com>
2021-10-28 10:16:34 +0100

    create routes file
    https://youtu.be/ZGymN8aFsv4?t=639

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// For this to work we need a .env file
+// API details from that file come from here https://openweathermap.org/current
+// Holds stuff like this
+// API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+// API_KEY_NAME = "appid"
+// API_KEY_VALUE = "[My Api Key]"
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -6,9 +13,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ success: true });
-});
+// Routes
+// since we want ./routes/index we can leave out the "index" bit
+app.use("/api", require("./routes"));
 
 // Enable cors
 app.use(cors());
